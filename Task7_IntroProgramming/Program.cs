f700a10eeea9f22dbf83197f75790bccd6f142da
vvlee <vvlee@gmail.com>
2022-12-15 23:25:02 +0500

    add Task 7 IntroProgramming

---
+++ Task7_IntroProgramming/Program.cs
@@ -0,0 +1,27 @@
+﻿int count = 0,
+    distance = 10000,
+    firstFriendSpeed = 1,
+    secondFriendSpeed = 2,
+    dogSpeed = 5,
+    total_time = 0;
+int friend = 2;
+
+    while(distance >10)
+    {
+        if(friend == 1)
+        {
+            total_time = distance / (firstFriendSpeed + dogSpeed);
+            friend = 2;
+        }
+        else
+        {
+            total_time = distance / (secondFriendSpeed + dogSpeed);
+            friend = 1;
+        }
+
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * total_time;
+        count++;
+    }
+
+    Console.Write("Собака пробежит - ");
+    Console.WriteLine(count);
